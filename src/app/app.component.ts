8fafa6e2bcbf70799336523eb1e470d0c01bc9c4
CariBosio <caribosio72@gmail.com>
2023-07-16 22:04:26 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+type Item = {
+  producto: string;
+  costo: string;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'tarea-angular';
+  items: Item[] = [
+    { producto: 'Limón', costo: '$ 1200' },
+    { producto: 'Tomate', costo: '$ 900' },
+    { producto: 'Zanahoria', costo: '$ 500' },
+    { producto: 'Cebolla', costo: '$ 300' },
+    { producto: 'Palta', costo: '$ 4000' },
+    { producto: 'Zapallo', costo: '$ 1000' },
+    { producto: 'Papa', costo: '$ 380' },
+    { producto: 'Lechuga', costo: '$ 400' }
+  ];
+
+  mostrarLista: boolean = true;
+
+  toggleLista(): void {
+    this.mostrarLista = !this.mostrarLista;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
